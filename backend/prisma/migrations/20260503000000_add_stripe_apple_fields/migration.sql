8293a2b3675bcd9e1d60a576bd19e003098b3ca4
細田一成 <hosodaissei@hosodaisseinoMacBook-Air.local>
2026-05-03 15:09:53 +0900

    Remove backend from tracking

--- backend/prisma/migrations/20260503000000_add_stripe_apple_fields/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "stripeCustomerId" TEXT;
-ALTER TABLE "User" ADD COLUMN "appleOriginalTransactionId" TEXT;
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_stripeCustomerId_key" ON "User"("stripeCustomerId");
-CREATE UNIQUE INDEX "User_subscriptionId_key" ON "User"("subscriptionId");
-CREATE UNIQUE INDEX "User_appleOriginalTransactionId_key" ON "User"("appleOriginalTransactionId");
--- backend/src/auth/skip-db-lookup.decorator.ts
+++
@@ -1,4 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const SKIP_DB_LOOKUP_KEY = 'skip_db_lookup';
-export const SkipDbLookup = () => SetMetadata(SKIP_DB_LOOKUP_KEY, true);
